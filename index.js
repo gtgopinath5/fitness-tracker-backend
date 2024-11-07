0c542d81ba22f22a75cde950bd8ec64914a77cb2
gtgopinath5 <gtgopinath5@gmail.com>
2024-11-07 12:41:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user/", UserRoutes);
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong";
+    return res.status(status).json({
+      success: false,
+      status,
+      message,
+    });
+});
+
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        message: "hello developers from GFG"
+    });
+});
+
+const connectDB = () => {
+    mongoose.set("strictQuery", true);
+    mongoose
+        .connect(process.env.MONGODB_URL)
+        .then(() => console.log("Connected to MongoDB"))
+        .catch((err) => {
+            console.log("MongoDB connection error:", err);
+        });
+};
+
+const startServer = async () => {
+    try {
+        connectDB();
+        app.listen(8080, () => console.log("Server running at port 8080"));
+    } catch (err) {
+        console.log("Server start error:", err);
+    }
+};
+
+startServer();
+
