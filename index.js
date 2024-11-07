154d0dc22bdbd6abbab177eb35ec61bf8e7a2507
gtgopinath5 <gtgopinath5@gmail.com>
2024-11-07 12:44:19 +0530

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://fitness-tracker-frontend-vert.vercel.app/' }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
